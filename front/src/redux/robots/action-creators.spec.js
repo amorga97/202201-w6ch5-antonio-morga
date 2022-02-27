3cbe38c7ea84e0ddbec3ecfe86cdc47b6bff417e
Antonio Morga <anotniomc9719@gmail.com>
2022-02-27 23:48:08 +0100

    Adapted the remaining front-end tests

--- front/src/redux/robots/action-creators.spec.js
+++ front/src/redux/robots/action-creators.spec.js
@@ -3,64 +3,64 @@ import * as api from '../../services/api';
 
 jest.mock('react-redux');
 jest.mock('../../services/api');
-const mockTask = { id: 1, title: 'do sth', isComplete: false };
+const mockRobot = { id: 1, title: 'do sth', isComplete: false };
 
 let dispatch;
 
-describe('Given the function loadTasks', () => {
+describe('Given the function loadRobots', () => {
   beforeEach(() => {
     dispatch = jest.fn();
   });
-  describe('When calling it with a task as a parameter', () => {
+  describe('When calling it with a Robot as a parameter', () => {
     test('Then it should return an action object', async () => {
-      api.getTasks.mockResolvedValue(mockTask);
+      api.getRobots.mockResolvedValue(mockRobot);
 
-      await action.loadTasks(mockTask)(dispatch);
+      await action.loadRobots(mockRobot)(dispatch);
 
       expect(dispatch).toHaveBeenCalled();
     });
   });
 });
 
-describe('Given the function addTask', () => {
+describe('Given the function addRobot', () => {
   beforeEach(() => {
     dispatch = jest.fn();
   });
-  describe('When calling it with a task as a parameter', () => {
+  describe('When calling it with a Robot as a parameter', () => {
     test('Then it should return an action object', async () => {
-      api.addTask.mockResolvedValue(mockTask);
+      api.addRobot.mockResolvedValue(mockRobot);
 
-      await action.addTask(mockTask)(dispatch);
+      await action.addRobot(mockRobot)(dispatch);
 
       expect(dispatch).toHaveBeenCalled();
     });
   });
 });
 
-describe('Given the function deleteTask', () => {
+describe('Given the function deleteRobot', () => {
   beforeEach(() => {
     dispatch = jest.fn();
   });
-  describe('When calling it with a task as a parameter', () => {
+  describe('When calling it with a Robot as a parameter', () => {
     test('Then it should return an action object', async () => {
-      api.deleteTask.mockResolvedValue();
+      api.deleteRobot.mockResolvedValue();
 
-      await action.deleteTask(mockTask)(dispatch);
+      await action.deleteRobot(mockRobot)(dispatch);
 
       expect(dispatch).toHaveBeenCalled();
     });
   });
 });
 
-describe('Given the function updateTask', () => {
+describe('Given the function updateRobot', () => {
   beforeEach(() => {
     dispatch = jest.fn();
   });
-  describe('When calling it with a task as a parameter', () => {
+  describe('When calling it with a Robot as a parameter', () => {
     test('Then it should return an action object', async () => {
-      api.updateTask.mockResolvedValue(mockTask);
+      api.updateRobot.mockResolvedValue(mockRobot);
 
-      await action.updateTask(mockTask)(dispatch);
+      await action.updateRobot(mockRobot)(dispatch);
 
       expect(dispatch).toHaveBeenCalled();
     });
--- front/src/redux/robots/action-types.spec.js
+++ front/src/redux/robots/action-types.spec.js
@@ -2,8 +2,8 @@ import { actionTypes } from './action-types';
 
 describe('Given the constant actionTypes', () => {
   test('It should have the following properties', () => {
-    expect(actionTypes.addTask).toBeDefined();
-    expect(actionTypes.deleteTask).toBeDefined();
-    expect(actionTypes.updateTask).toBeDefined();
+    expect(actionTypes.addRobot).toBeDefined();
+    expect(actionTypes.deleteRobot).toBeDefined();
+    expect(actionTypes.updateRobot).toBeDefined();
   });
 });
