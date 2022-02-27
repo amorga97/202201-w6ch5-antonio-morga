d5e62c5d1b631a1d1b8bbaa8f0dee792816ac189
Antonio Morga <anotniomc9719@gmail.com>
2022-02-27 12:25:46 +0100

    Updated form component to be able to introduce a robot wiith all necessary data

--- front/src/components/form.js
+++ front/src/components/form.js
@@ -3,21 +3,25 @@ import * as action from '../redux/robots/action-creators';
 import { useDispatch } from 'react-redux';
 
 export function Form() {
-  const [formState, setFormState] = useState({
-    title: '',
-    isComplete: false,
-  });
+  const initialState = {
+    robotName: '',
+    speed: 0,
+    strength: 0,
+    creationDate: '',
+    img: '',
+  };
+  const [formState, setFormState] = useState(initialState);
 
   const dispatch = useDispatch();
 
   const handleChange = (ev) => {
-    setFormState({ ...formState, title: ev.target.value });
+    setFormState({ ...formState, [ev.target.name]: ev.target.value });
   };
 
   const handleSubmit = (ev) => {
     ev.preventDefault();
     dispatch(action.addRobot(formState));
-    setFormState({ title: '', isComplete: false });
+    setFormState(initialState);
   };
 
   return (
@@ -25,13 +29,50 @@ export function Form() {
       <input
         className="form__input"
         type="text"
-        name="title"
-        placeholder="new task"
+        name="robotName"
+        placeholder="Robot name"
         required
-        value={formState.title}
+        value={formState.robotName}
         onChange={handleChange}
       />
-      <input type="submit" value="create task" />
+      <label htmlFor="speed">Speed</label>
+      <input
+        type="range"
+        name="speed"
+        id="speed"
+        value={formState.speed}
+        min="0"
+        max="10"
+        step="1"
+        onChange={handleChange}
+      />
+      <label htmlFor="strength">Strength</label>
+      <input
+        type="range"
+        name="strength"
+        id="strength"
+        value={formState.strength}
+        min="0"
+        max="10"
+        step="1"
+        onChange={handleChange}
+      />
+      <input
+        className="form__input"
+        type="text"
+        name="img"
+        placeholder="Picture url"
+        value={formState.img}
+        onChange={handleChange}
+      />
+      <input
+        type="date"
+        name="creationDate"
+        id=""
+        value={formState.creationDate}
+        onChange={handleChange}
+      />
+      <input type="submit" value="create robot" />
     </form>
   );
 }
